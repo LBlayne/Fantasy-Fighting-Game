80007817425db46203d5829cab7e807d15814a61
Logan Marquez <marquezlogan6@gmail.com>
2022-04-28 13:38:41 -0700

    Refined the fighting UI

--- main.cpp
+++ main.cpp
@@ -25,39 +25,44 @@ int main() {
 
 		int vDamage = rand() % 5 + 1;
 
-		cout << "Attack (y/n)" << endl;
+		cout << "\nAttack (y/n) or press 1 to stop" << endl;
 		std::cin >> in;
 
 		if (in == "y") {
 			
 			//villian & hero stats
-			cout << "enemy health = " << villian.getHealth() << endl;
-			cout << "hero health = " << hero.getHealth() << endl;
+			cout << "\n\tenemy health = " << villian.getHealth() << endl;
+			cout << "	hero health = " << hero.getHealth() << endl;
 
-		
-			villian.setHealth(villian.getHealth() - w->getDamage()); //hero attacks
-
-			cout << "\n\n The villian is attacking \n" << endl; //villian attacks
+			//user Attacks
+			cout << "\n you attack for " << w->getDamage() << " damage\n" << endl;
+			villian.setHealth(villian.getHealth() - w->getDamage()); 
+			cout << "	enemy health = " << villian.getHealth() << endl;
+	
+			//Enemy Attacks
+			cout << "\n\n The villian is attacking for " << vDamage << " damage\n" << endl;
 			hero.setHealth(hero.getHealth() - vDamage);
-
-			cout << "enemy health = " << villian.getHealth() << endl;
-			cout << "hero health = " << hero.getHealth() << endl;
+			cout << "	hero health = " << hero.getHealth() << endl;
 		}
 
 		// "no" means villian attacks
 		else if (in == "n") {
 
-			
-			cout << "\n\n The villian is attacking" << endl;
+			cout << "\n\n The villian is attacking for " << vDamage << " damage\n" << endl;
 			hero.setHealth(hero.getHealth() - vDamage);
 
-			cout << "enemy health = " << villian.getHealth() << endl;
-			cout << "hero health = " << hero.getHealth() << endl;
+			cout << "	hero health = " << hero.getHealth() << endl;
+		}
+
+		else if (in == "1")
+		{
+			cout << "\n this is a battle, you can't just stop!! Get a grip \n" << endl;
 		}
 
 		else { 
+
 			//user is a little slow
-			cout << "you can only enter y/n" << endl;
+			cout << "\nyou can only enter y/n" << endl;
 		}
 	}
 	
